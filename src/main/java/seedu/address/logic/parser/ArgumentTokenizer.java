b1a63c5c9d29bb82cefb944d4b7952f4ee5b98e8
calijacked <162989170+calijacked@users.noreply.github.com>
2026-03-25 03:44:00 +0800

    Add test cases for making case insensitive and change Prefix class methods to accomodate lowercase handling

--- src/main/java/seedu/address/logic/parser/ArgumentTokenizer.java
+++ src/main/java/seedu/address/logic/parser/ArgumentTokenizer.java
@@ -47,12 +47,13 @@ public class ArgumentTokenizer {
     private static List<PrefixPosition> findPrefixPositions(String argsString, Prefix prefix) {
         List<PrefixPosition> positions = new ArrayList<>();
         String argsStringLower = argsString.toLowerCase();
+        String prefixLower = prefix.getPrefix().toLowerCase();
 
-        int prefixPosition = findPrefixPosition(argsStringLower, prefix.getPrefix(), 0);
+        int prefixPosition = findPrefixPosition(argsStringLower, prefixLower, 0);
         while (prefixPosition != -1) {
             PrefixPosition extendedPrefix = new PrefixPosition(prefix, prefixPosition);
             positions.add(extendedPrefix);
-            prefixPosition = findPrefixPosition(argsStringLower, prefix.getPrefix(), prefixPosition);
+            prefixPosition = findPrefixPosition(argsStringLower, prefixLower, prefixPosition);
         }
 
         return positions;
